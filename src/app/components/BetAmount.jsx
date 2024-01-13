19fc248d994656408a58b1cde492f15c29f8c56e
Musadiq Khan <musadiq@nullbrainer.io>
2024-01-13 05:22:53 +0500

    updates

--- src/app/components/BetAmount.jsx
+++ src/app/components/BetAmount.jsx
@@ -1,31 +1,71 @@
+import React, { useState } from "react";
 import PreviousRolls from "./PreviousRolls";
 
 const BetAmount = () => {
-	return (
-		<div className="w-100">
-			<PreviousRolls />
-			<div className="bet-amount-wrapper my-3 mx-auto">
-				<img src={"/images/currency-big.svg"} alt="$" />
-				<input
-					className="bet-input mx-2"
-					type="number"
-					placeholder="Enter bet amount..."
-				/>
-				<div className="divider" />
-				<div className="badge-wrapper">
-					<div className="bet-badge">Clear</div>
-					<div className="bet-badge">+0.01</div>
-					<div className="bet-badge">+0.1</div>
-					<div className="bet-badge">+1</div>
-					<div className="bet-badge">+10</div>
-					<div className="bet-badge">+100</div>
-					<div className="bet-badge">1/2</div>
-					<div className="bet-badge">X2</div>
-					<div className="bet-badge">MAX</div>
-				</div>
-			</div>
-		</div>
-	);
+  const [number, setNumber] = useState(0.0);
+  return (
+    <div className="w-100">
+      <PreviousRolls />
+      <div className="bet-amount-wrapper my-3 mx-auto">
+        <img src={"/images/currency-big.svg"} alt="$" />
+        <input
+          className="bet-input mx-2"
+          type="number"
+          value={number}
+          placeholder="Enter bet amount..."
+        />
+        <div className="divider" />
+        <div className="badge-wrapper">
+          <div className="bet-badge cpointer" onClick={() => setNumber(0.0)}>
+            Clear
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber + 0.01)}
+          >
+            +0.01
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber + 0.1)}
+          >
+            +0.1
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber + 1)}
+          >
+            +1
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber + 10)}
+          >
+            +10
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber + 100)}
+          >
+            +100
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber / 2)}
+          >
+            1/2
+          </div>
+          <div
+            className="bet-badge cpointer"
+            onClick={() => setNumber((prevNumber) => prevNumber * 2)}
+          >
+            X2
+          </div>
+          <div className="bet-badge cpointer">MAX</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BetAmount;
--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -5,24 +5,24 @@ import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
-	const { width } = window.visualViewport;
-	const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
-	const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
+  const { width } = window.visualViewport;
+  const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
+  const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
 
-	return (
-		<div className="wrapper">
-			<FlipperNavbar
-				sidebar={sidebar}
-				setSidebar={setSidebar}
-				windowSize={windowSize}
-				setWindowSize={setWindowSize}
-			/>
-			<div className="wrapper-sub-topbar w-100 d-flex">
-				<FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <FlipperNavbar
+        sidebar={sidebar}
+        setSidebar={setSidebar}
+        windowSize={windowSize}
+        setWindowSize={setWindowSize}
+      />
+      <div className="wrapper-sub-topbar w-100 d-flex">
+        <FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
