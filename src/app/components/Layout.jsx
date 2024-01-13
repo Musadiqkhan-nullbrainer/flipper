49c18377927cbe4fa448a498be3b2fc69727eb07
Musadiq Khan <musadiq@nullbrainer.io>
2024-01-13 05:29:24 +0500

    window issue fixed

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -1,14 +1,17 @@
 "use client";
 import FlipperSideBar from "./FlipperSideBar";
 import FlipperNavbar from "./FlipperNavbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
-  const { width } = window.visualViewport;
+  const [width, setWidth] = useState("");
   const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
   const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
-
+  useEffect(() => {
+    const { width } = window.visualViewport;
+    setWidth(width);
+  }, []);
   return (
     <div className="wrapper">
       <FlipperNavbar
