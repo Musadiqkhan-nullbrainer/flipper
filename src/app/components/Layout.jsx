c700d88a6fc788ffd925e5c80358f2f753368406
Musadiq Khan <musadiq@nullbrainer.io>
2024-01-15 23:16:16 +0500

    Update Layout.jsx

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -5,33 +5,33 @@ import { useState, useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
-	const [width, setWidth] = useState("");
-	const [sidebar, setSidebar] = useState(
-		window.innerWidth <= 1000 ? false : true
-	);
-	const [windowSize, setWindowSize] = useState(
-		window.innerWidth <= 1000 ? true : false
-	);
-	useEffect(() => {
-		const { width } = window.visualViewport;
-		setSidebar(width <= 1000 ? false : true);
-		setWidth(width <= 1000 ? true : false);
-		setWidth(width);
-	}, []);
-	return (
-		<div className="wrapper">
-			<FlipperNavbar
-				sidebar={sidebar}
-				setSidebar={setSidebar}
-				windowSize={windowSize}
-				setWindowSize={setWindowSize}
-			/>
-			<div className="wrapper-sub-topbar w-100 d-flex">
-				<FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
-				{children}
-			</div>
-		</div>
-	);
+  const [width, setWidth] = useState("");
+  const [sidebar, setSidebar] = useState(
+    width?.innerWidth <= 1000 ? false : true
+  );
+  const [windowSize, setWindowSize] = useState(
+    width?.innerWidth <= 1000 ? true : false
+  );
+  useEffect(() => {
+    const { width } = window.visualViewport;
+    setSidebar(width <= 1000 ? false : true);
+    setWidth(width <= 1000 ? true : false);
+    setWidth(width);
+  }, []);
+  return (
+    <div className="wrapper">
+      <FlipperNavbar
+        sidebar={sidebar}
+        setSidebar={setSidebar}
+        windowSize={windowSize}
+        setWindowSize={setWindowSize}
+      />
+      <div className="wrapper-sub-topbar w-100 d-flex">
+        <FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
