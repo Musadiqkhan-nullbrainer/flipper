94846b9aeace0f8fb2dc4885cc662f9b98c44604
Musadiq Khan <musadiq@nullbrainer.io>
2024-01-16 01:54:15 +0500

    changes

--- src/app/components/PreviousRolls.jsx
+++ src/app/components/PreviousRolls.jsx
@@ -1,33 +1,57 @@
 import React, { useState, useEffect } from "react";
 const PreviousRolls = () => {
-	const [number, setNumber] = useState(550);
+  const [number, setNumber] = useState(550);
 
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			setNumber(40);
-		}, 12450); // 12.45 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setNumber(40);
+    }, 12450); // 12.45 seconds
 
-		return () => clearTimeout(timeout);
-	}, [number]);
+    return () => clearTimeout(timeout);
+  }, [number]);
 
-	return (
-		<div className="previous-roll-wrapper">
-			<h4 className="rolls-heading mb-0">Previous rolls</h4>
-			<div className="d-flex alig-items-center justofy-content-center flex-wrap">
-				<img src={"/images/gray-coin.svg"} alt="coin" className="mr-1 ml-2" />
-				<img src={"/images/gray-coin.svg"} alt="coin" className="mr-1" />
+  const [icons, setIcons] = useState([...Array(10).keys()]);
 
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/gray-coin.svg"} alt="coin" className="mr-1" />
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIcons((prevIcons) => {
+        let newIcons = [...prevIcons];
+        let movedIcon = newIcons.shift(); // Remove the first icon
+        newIcons.push(movedIcon); // Add it to the end
+        return newIcons;
+      });
+    }, 3000); // Change every 3 seconds
 
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-1" />
-				<img src={"/images/golden-coin.svg"} alt="coin" className="mr-2" />
-			</div>
-			{/* <div
+    return () => clearInterval(timer);
+  }, []);
+
+  const getIconStyle = (index, iconNumber) => {
+    const position = index * 10; // Position each icon 20px apart
+    const isMovingIcon = iconNumber === icons[0];
+    return {
+      transform: `translateX(${position}px)`,
+      opacity: isMovingIcon ? 0 : 1, // Hide the moving icon
+      backgroundImage: `url(${
+        iconNumber % 2 == 0
+          ? "/images/golden-coin.svg"
+          : "/images/gray-coin.svg"
+      })`,
+    };
+  };
+
+  return (
+    <div className="previous-roll-wrapper">
+      <h4 className="rolls-heading mb-0">Previous rolls</h4>
+      <div className="d-flex alig-items-center justify-content-center icon-container position-relative flex-wrap">
+        {icons.map((iconNumber, index) => (
+          <div
+            key={iconNumber}
+            className="icon"
+            style={getIconStyle(index, iconNumber)}
+          ></div>
+        ))}
+      </div>
+      {/* <div
         className="previous-roll position-relative mb-0 mx-3"
         style={{
           transition: "none",
@@ -36,17 +60,17 @@ const PreviousRolls = () => {
       >
         <div className="mask position-absolute z-3 "></div>
       </div> */}
-			<div className="d-flex align-items-center right-coins">
-				<h4 className="rolls-heading mb-0 mx-3">Last 100</h4>
-				<img src={"/images/Coin1.svg"} alt="coin" className="mx-1" />
-				<h4 className="mb-0 coin-heading">40</h4>
-				<img src={"/images/Coin2.svg"} alt="coin" className="mx-1" />
-				<h4 className="mb-0 coin-heading">4</h4>
-				<img src={"/images/Coin3.svg"} alt="coin" className="mx-1" />
-				<h4 className="mb-0 coin-heading">56</h4>
-			</div>
-		</div>
-	);
+      <div className="d-flex align-items-center right-coins">
+        <h4 className="rolls-heading mb-0 mx-3">Last 100</h4>
+        <img src={"/images/Coin1.svg"} alt="coin" className="mx-1" />
+        <h4 className="mb-0 coin-heading">40</h4>
+        <img src={"/images/Coin2.svg"} alt="coin" className="mx-1" />
+        <h4 className="mb-0 coin-heading">4</h4>
+        <img src={"/images/Coin3.svg"} alt="coin" className="mx-1" />
+        <h4 className="mb-0 coin-heading">56</h4>
+      </div>
+    </div>
+  );
 };
 
 export default PreviousRolls;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,104 +10,105 @@ import more from "@public/images/more.svg";
 let seq = 0;
 let seqs = 1;
 const Home = () => {
-	const [number, setNumber] = useState(550);
-	const [hide, setHide] = useState(false);
-	const [counter, setCounter] = useState(12.45);
-	const [list, setList] = useState([]);
-	const [lists, setLists] = useState([]);
+  const [number, setNumber] = useState(550);
+  const [hide, setHide] = useState(false);
+  const [counter, setCounter] = useState(12.45);
+  const [list, setList] = useState([]);
+  const [lists, setLists] = useState([]);
 
-	useEffect(() => {
-		setTimeout(() => {
-			const newList = [
-				...list,
-				{ id: seq + Math.floor(Math.random() * 25) + 1 },
-			];
-			newList.sort((a, b) => b.id - a.id);
-			setList(newList);
-			const newLists = [
-				...list,
-				{ id: seq + Math.floor(Math.random() * 3) + 1 },
-			];
-			newLists.sort((a, b) => b.id - a.id);
-			setLists(newLists);
-		}, 25000);
-	}, [list, lists]);
+  useEffect(() => {
+    setTimeout(() => {
+      const newList = [
+        ...list,
+        { id: seq + Math.floor(Math.random() * 25) + 1 },
+      ];
+      newList.sort((a, b) => b.id - a.id);
+      setList(newList);
+      const newLists = [
+        ...list,
+        { id: seq + Math.floor(Math.random() * 3) + 1 },
+      ];
+      newLists.sort((a, b) => b.id - a.id);
+      setLists(newLists);
+    }, 25000);
+  }, [list, lists]);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setCounter((prevCounter) => {
-				if (prevCounter <= 0) {
-					setHide(true);
-					setNumber((prevNumber) => prevNumber * 1.05);
-					return 0;
-				}
-				setHide(false);
-				return prevCounter - 0.1;
-			});
-		}, 80);
-		return () => clearInterval(interval);
-	}, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => {
+        if (prevCounter <= 0) {
+          setHide(true);
+          setNumber((prevNumber) => prevNumber * 1.05);
+          return 0;
+        }
+        setHide(false);
+        return prevCounter - 0.1;
+      });
+    }, 80);
+    return () => clearInterval(interval);
+  }, []);
 
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			if (counter <= 0) {
-				setNumber(550);
-				setCounter(12.45);
-			}
-		}, 12450); // 12.45 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (counter <= 0) {
+        setNumber(550);
+        setCounter(12.45);
+      }
+    }, 12450); // 12.45 seconds
 
-		return () => clearTimeout(timeout);
-	}, [counter]);
+    return () => clearTimeout(timeout);
+  }, [counter]);
 
-	return (
-		<Layout>
-			<div className="details-wrapper">
-				<div className="detail-box">
-					<div className="row">
-						<div className="col-8">
-							<p className="title text-uppercase">Roulette</p>
-						</div>
-						<div className="col-4 text-right ">
-							<img src={"/images/soundon.svg"} />{" "}
-							<span className="sound-text">Sound on</span>
-						</div>
-					</div>
-					<div
-						className="roll d-flex align-items-center position-relative mb-4"
-						style={{
-							transition: "none",
-							backgroundPosition: `${number}px center`,
-						}}>
-						{!hide && (
-							<div className="d-flex position-relative w-100 justify-content-center flex-column align-items-center">
-								<div className="rolling">ROLLING</div>
-								<div className="count">{counter.toFixed(2)}</div>
-							</div>
-						)}
-						{!hide && <div className="mask position-absolute z-3 "></div>}
-					</div>
+  return (
+    <Layout>
+      <div className="details-wrapper">
+        <div className="detail-box">
+          <div className="row">
+            <div className="col-8">
+              <p className="title text-uppercase">Roulette</p>
+            </div>
+            <div className="col-4 text-right ">
+              <img src={"/images/soundon.svg"} />{" "}
+              <span className="sound-text">Sound on</span>
+            </div>
+          </div>
+          <div
+            className="roll d-flex align-items-center position-relative mb-4"
+            style={{
+              transition: "none",
+              backgroundPosition: `${number}px center`,
+            }}
+          >
+            {!hide && (
+              <div className="d-flex position-relative w-100 justify-content-center flex-column align-items-center">
+                <div className="rolling">ROLLING</div>
+                <div className="count">{counter.toFixed(2)}</div>
+              </div>
+            )}
+            {!hide && <div className="mask position-absolute z-3 "></div>}
+          </div>
 
-					<BetAmount />
+          <BetAmount />
 
-					<div className="card-wrapper flex-md-wrap card-wrapper-padding">
-						<div className="card-container mb-lg-0 mb-5">
-							<CardBars />
-							<Cards list={lists} id="first" />
-						</div>
-						<div className="card-container mb-lg-0 mb-5">
-							<CardBars />
-							<Cards list={list} id="second" />
-						</div>
-						<div className="card-container mb-lg-0 mb-5">
-							<CardBars />
-							<Cards list={list} id="third" />
-						</div>
-					</div>
-				</div>
-				<Table />
-			</div>
-		</Layout>
-	);
+          <div className="card-wrapper flex-md-wrap card-wrapper-padding">
+            <div className="card-container mb-lg-0 mb-5">
+              <CardBars />
+              <Cards list={lists} id="first" />
+            </div>
+            <div className="card-container mb-lg-0 mb-5">
+              <CardBars />
+              <Cards list={list} id="second" />
+            </div>
+            <div className="card-container mb-lg-0 mb-5">
+              <CardBars />
+              <Cards list={list} id="third" />
+            </div>
+          </div>
+        </div>
+        <Table />
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
