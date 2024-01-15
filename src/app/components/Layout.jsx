3b03bdcd2659f5aa8f8f82be344b156d5db7aa3c
wahab-ahmad-khan <wahab@nullbrainer.io>
2024-01-15 14:19:41 +0500

    pull

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -5,29 +5,29 @@ import { useState, useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
-  const [width, setWidth] = useState("");
-  const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
-  const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
-  useEffect(() => {
-    const { width } = window.visualViewport;
-    setSidebar(width <= 1000 ? false : true);
-    setWidth(width <= 1000 ? true : false);
-    setWidth(width);
-  }, []);
-  return (
-    <div className="wrapper">
-      <FlipperNavbar
-        sidebar={sidebar}
-        setSidebar={setSidebar}
-        windowSize={windowSize}
-        setWindowSize={setWindowSize}
-      />
-      <div className="wrapper-sub-topbar w-100 d-flex">
-        <FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
-        {children}
-      </div>
-    </div>
-  );
+	const [width, setWidth] = useState("");
+	const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
+	const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
+	useEffect(() => {
+		const { width } = window.visualViewport;
+		setSidebar(width <= 1000 ? false : true);
+		setWidth(width <= 1000 ? true : false);
+		setWidth(width);
+	}, []);
+	return (
+		<div className="wrapper">
+			<FlipperNavbar
+				sidebar={sidebar}
+				setSidebar={setSidebar}
+				windowSize={windowSize}
+				setWindowSize={setWindowSize}
+			/>
+			<div className="wrapper-sub-topbar w-100 d-flex">
+				<FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
+				{children}
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
---
+++ src/app/components/Messages.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const Messages = () => {
+	return (
+		<div className="messages">
+			<>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "red" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "red" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "red" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "red" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "#AA9987" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "#534235" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-3">
+					<p className="m-0 p-0 d-flex align-items-center ">
+						<img className="mr-2" src={"/images/bitmoji.svg"} alt="F" />
+						<p className="name mr-2"> FenRik</p>
+						<div
+							className="color mr-2"
+							style={{ backgroundColor: "red" }}></div>
+						<p className="payment mr-2">ez money</p>
+					</p>
+				</div>
+				<div className="mt-4 messages-input d-flex align-items-center justify-content-between">
+					<input className="" type="text" placeholder="Type Message" />
+					<img src="/images/more.svg" className="cpointer" />
+				</div>
+			</>
+		</div>
+	);
+};
+
+export default Messages;
