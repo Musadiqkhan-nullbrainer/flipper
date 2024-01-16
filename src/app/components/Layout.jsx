18b573060ee036f8c9b80964368d5b7be9fefa12
Musadiq Khan <musadiq@nullbrainer.io>
2024-01-16 16:29:22 +0500

    Update Layout.jsx

--- src/app/components/Layout.jsx
+++ src/app/components/Layout.jsx
@@ -6,47 +6,46 @@ import { SuspenseLoading } from "./SuspenseLoading";
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
-	const [width, setWidth] = useState(1000);
-	const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
-	const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
-	const [isLoading, setIsLoading] = useState(true);
+  const [width, setWidth] = useState(1000);
+  const [sidebar, setSidebar] = useState(width <= 1000 ? false : true);
+  const [windowSize, setWindowSize] = useState(width <= 1000 ? true : false);
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		const { width } = window.visualViewport;
-		setSidebar(width <= 1000 ? false : true);
-		setWidth(width);
+  useEffect(() => {
+    const { width } = window.visualViewport;
+    setSidebar(width <= 1000 ? false : true);
+    setWidth(width);
 
-		// Simulate a 2-second loading delay
-		const timeoutId = setTimeout(() => {
-			setIsLoading(false);
-		}, 2000);
+    // Simulate a 2-second loading delay
+    if (window) {
+      setIsLoading(false);
+    }
 
-		// Clear the timeout when the component unmounts
-		return () => clearTimeout(timeoutId);
-	}, []);
+    // Clear the timeout when the component unmounts
+  }, []);
 
-	return (
-		<div className="wrapper">
-			<Suspense fallback={<SuspenseLoading />}>
-				{isLoading ? (
-					<SuspenseLoading />
-				) : (
-					<>
-						<FlipperNavbar
-							sidebar={sidebar}
-							setSidebar={setSidebar}
-							windowSize={windowSize}
-							setWindowSize={setWindowSize}
-						/>
-						<div className="wrapper-sub-topbar w-100 d-flex">
-							<FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
-							{children}
-						</div>
-					</>
-				)}
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <Suspense fallback={<SuspenseLoading />}>
+        {isLoading ? (
+          <SuspenseLoading />
+        ) : (
+          <>
+            <FlipperNavbar
+              sidebar={sidebar}
+              setSidebar={setSidebar}
+              windowSize={windowSize}
+              setWindowSize={setWindowSize}
+            />
+            <div className="wrapper-sub-topbar w-100 d-flex">
+              <FlipperSideBar sidebar={sidebar} windowSize={windowSize} />
+              {children}
+            </div>
+          </>
+        )}
+      </Suspense>
+    </div>
+  );
 };
 
 export default Layout;
